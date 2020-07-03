e384ed03afed177fa4ce1be6c45f6c0f96ac9719
Leo Yan <leo.yan@linaro.org>
2020-07-03 06:28:23 -0400

    test: multi host testing: Test for LV lock (sh and ex)
    Signed-off-by: Leo Yan <leo.yan@linaro.org>

---
+++ test/shell/idm_multi_hosts_lv_sh_ex_hosta.sh
@@ -0,0 +1,45 @@
+SKIP_WITH_LVMPOLLD=1
+
+. lib/inittest
+
+[ -z "$LVM_TEST_LOCK_TYPE_IDM" ] && skip;
+[ -z "$LVM_TEST_MULTI_HOST_IDM" ] && skip;
+
+aux extend_filter_LVMTEST "a|/dev/sdb*|" "a|/dev/sdc*|" "a|/dev/sdd*|"
+aux lvmconf "devices/allow_changes_with_duplicate_pvs = 1"
+
+BLKS=("/dev/sdb2" "/dev/sdb3" "/dev/sdb4" "/dev/sdb5"
+      "/dev/sdc2" "/dev/sdc3" "/dev/sdc4" "/dev/sdc5"
+      "/dev/sdd2" "/dev/sdd3" "/dev/sdd4" "/dev/sdd5")
+
+for d in "${BLKS[@]}"; do
+	dd if=/dev/zero of="$d" bs=1MB count=1000
+	wipefs -a "$d" 2>/dev/null || true
+done
+
+i=0
+for d in "${BLKS[@]}"; do
+	i=$((i+1))
+	dmsetup remove /dev/TESTVG$i/foo || true
+	dmsetup remove /dev/TESTVG$i || true
+done
+
+i=0
+for d in "${BLKS[@]}"; do
+	i=$((i+1))
+	vgcreate --shared --locktype idm TESTVG$i $d
+	lvcreate -a n --zero n -l 1 -n foo TESTVG$i
+	lvchange -a sy TESTVG$i/foo
+done
+
+i=0
+for d in "${BLKS[@]}"; do
+	i=$((i+1))
+	lvchange -a ey TESTVG$i/foo
+done
+
+i=0
+for d in "${BLKS[@]}"; do
+	i=$((i+1))
+	lvchange -a n TESTVG$i/foo
+done
---
+++ test/shell/idm_multi_hosts_lv_sh_ex_hostb.sh
@@ -0,0 +1,36 @@
+SKIP_WITH_LVMPOLLD=1
+
+. lib/inittest
+
+[ -z "$LVM_TEST_LOCK_TYPE_IDM" ] && skip;
+[ -z "$LVM_TEST_MULTI_HOST_IDM" ] && skip;
+
+aux extend_filter_LVMTEST "a|/dev/sdb*|" "a|/dev/sdc*|" "a|/dev/sdd*|"
+aux lvmconf "devices/allow_changes_with_duplicate_pvs = 1"
+
+BLKS=("/dev/sdb2" "/dev/sdb3" "/dev/sdb4" "/dev/sdb5"
+      "/dev/sdc2" "/dev/sdc3" "/dev/sdc4" "/dev/sdc5"
+      "/dev/sdd2" "/dev/sdd3" "/dev/sdd4" "/dev/sdd5")
+
+vgchange --lock-start
+
+vgdisplay
+
+i=0
+for d in "${BLKS[@]}"; do
+        i=$((i+1))
+	lvchange -a sy TESTVG$i/foo
+done
+
+i=0
+for d in "${BLKS[@]}"; do
+        i=$((i+1))
+	lvchange -a ey TESTVG$i/foo
+	lvchange -a n TESTVG$i/foo
+done
+
+i=0
+for d in "${BLKS[@]}"; do
+        i=$((i+1))
+	vgremove -f TESTVG$i
+done
