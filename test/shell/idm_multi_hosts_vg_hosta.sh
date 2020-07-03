b99667788cd7c704e8b376569b195dafc1654c1a
Leo Yan <leo.yan@linaro.org>
2020-07-02 22:30:48 -0400

    test: multi host testing: Test for VG
    Signed-off-by: Leo Yan <leo.yan@linaro.org>

---
+++ test/shell/idm_multi_hosts_vg_hosta.sh
@@ -0,0 +1,25 @@
+SKIP_WITH_LVMPOLLD=1
+
+. lib/inittest
+
+[ -z "$LVM_TEST_LOCK_TYPE_IDM" ] && skip;
+[ -z "$LVM_TEST_MULTI_HOST_IDM" ] && skip;
+
+aux extend_filter_LVMTEST "a|/dev/sdb*|" "a|/dev/sdc*|" "a|/dev/sdd*|"
+
+BLKS=("/dev/sdb2" "/dev/sdb3" "/dev/sdb4" "/dev/sdb5"
+      "/dev/sdc2" "/dev/sdc3" "/dev/sdc4" "/dev/sdc5"
+      "/dev/sdd2" "/dev/sdd3" "/dev/sdd4" "/dev/sdd5")
+
+for d in "${BLKS[@]}"; do
+	dd if=/dev/zero of="$d" bs=1MB count=1000
+	wipefs -a "$d" 2>/dev/null || true
+done
+
+i=0
+for d in "${BLKS[@]}"; do
+	echo $i
+	i=$((i+1))
+	vgcreate --shared --locktype idm TESTVG$i $d
+	vgchange -a n TESTVG$i
+done
